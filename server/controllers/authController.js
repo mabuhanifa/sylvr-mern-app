4aaaddc0293498d72a3b5bb38afed812ade85916
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-08 01:27:00 +0600

    express app setup

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+const loginController = async (req, res) => {
+    try {
+      const { name } = req.body;
+  
+      res.send(`Hello ${name}`);
+    } catch (error) {
+      res.send({ error });
+    }
+  };
+  
+  module.exports = { loginController };
